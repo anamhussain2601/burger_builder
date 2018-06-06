04ca8198174526f20af4155860a88f425335e7cb
anamhussain <anamhussain2601@gmail.com>
2018-06-06 23:54:30 +0530

    code refactoring

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Aux from '../../hoc/Aux'
+import AuxNew from '../../hoc/AuxNew'
 import classes from './layout.css';
 
 const layout = (props) =>(
-    <Aux>
+    <AuxNew>
     <div>Toolbar ,SideDrawer, Backdrop   </div>
     <main className = {classes.Content }>
         {props.children}
     </main>
-    </Aux>
+    </AuxNew>
 );
 
 export default layout; 
