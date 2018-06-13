3cae9f5685fcb0c30710c8f82bbc3511f917f81f
anamhussain <anamhussain2601@gmail.com>
2018-06-13 22:41:22 +0530

    App made responsive and Logo implemented

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import AuxNew from '../../hoc/AuxNew'
 import classes from './layout.css';
 import ToolBar from '../Navigation/ToolBar/ToolBar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) =>(
     <AuxNew>
     {/* <div>Toolbar ,SideDrawer, Backdrop   </div> */}
     <ToolBar/>
+    <SideDrawer/>
     <main className = {classes.Content }>
         {props.children}
     </main>
